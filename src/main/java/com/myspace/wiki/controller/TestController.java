98a677b7c3f1b56bbd43d5522d05ef278b451b3f
WildThorn <1343578324@qq.com>
2021-11-30 14:04:12 +0800

    SpringBoot配置文件 自定义配置项 (￣▽￣)／

--- src/main/java/com/myspace/wiki/controller/TestController.java
+++ src/main/java/com/myspace/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.myspace.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello:TEST}")
+    private String testHello;
     /**
      * Http 请求方式 ：GET,POST,PUT,DELETE
      * @RequestMapping注解支持所有的请求方式
@@ -25,7 +28,7 @@ public class TestController {
      */
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
 
     @PostMapping("/hello/post")

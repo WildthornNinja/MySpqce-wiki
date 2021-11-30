8ef550df37e5e0a74a323940a255afaeeac7a675
WildThorn <1343578324@qq.com>
2021-11-30 17:04:05 +0800

    增加demo表列表查询 ε≡٩(๑>₃<)۶

---
+++ src/main/java/com/myspace/wiki/controller/DemoController.java
@@ -0,0 +1,23 @@
+package com.myspace.wiki.controller;
+
+import com.myspace.wiki.service.DemoService;
+import com.myspace.wiki.domain.Demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @Resource
+    private DemoService demoService;
+
+    @GetMapping("/list")
+    public List<Demo> list() {
+        return demoService.list();
+    }
+}
---
+++ src/main/java/com/myspace/wiki/service/DemoService.java
@@ -0,0 +1,19 @@
+package com.myspace.wiki.service;
+
+import com.myspace.wiki.domain.Demo;
+import com.myspace.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list() {
+        return demoMapper.selectByExample(null);
+    }
+}

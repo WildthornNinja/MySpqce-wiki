87494f2cd040e578e3c3840950f20fc9ec9ec145
WildThorn <1343578324@qq.com>
2021-12-08 07:47:43 +0800

    制作the-footer组件(ૢ˃ꌂ˂⁎)

--- web/src/App.vue
+++ web/src/App.vue
@@ -4,21 +4,21 @@
 
       <router-view/>
 
+    <the-footer></the-footer>
 
-    <a-layout-footer style="text-align: center">
-      野棘忍のwiki
-    </a-layout-footer>
   </a-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TheHeader from '@/components/the-header.vue';
+import TheFooter from '@/components/the-footer.vue';
 
 export default defineComponent({
   name: 'app',
   components: {
     TheHeader,
+    TheFooter,
   },
 });
 </script>

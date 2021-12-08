3ac37336edfde54ff7084e242cbebfaaa7621586
WildThorn <1343578324@qq.com>
2021-12-08 10:18:08 +0800

    axios请求域改为读配置文件( ﾟ∀ﾟ)

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -98,8 +98,8 @@ export default defineComponent({
     const ebooklist = reactive({books :[]});
 
     onMounted(()=>{
-      console.log("onMounted");
-      axios.get("http://127.0.0.1:8880/ebook/list").then((response) =>{
+      console.log("onMounted————");
+      axios.get(process.env.VUE_APP_SERVER+"/ebook/list").then((response) =>{
         const data = response.data;
         ebooks.value = data.content;
         ebooklist.books = data.content;

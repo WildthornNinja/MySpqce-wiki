391434451f4469be1a299e468314400fb5a7f7c8
WildThorn <1343578324@qq.com>
2021-12-08 15:02:01 +0800

    点击编辑按钮时,弹出模态框♪ヾ(✿ﾟ▽ﾟ)ノ

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -16,7 +16,7 @@
         </template>
         <template v-slot:action="{ text, record }">
           <a-space size="small">
-            <a-button type="primary">
+            <a-button type="primary"  @click="edit">
               编辑
             </a-button>
             <a-button type="danger">
@@ -27,6 +27,14 @@
       </a-table>
     </a-layout-content>
   </a-layout>
+  <a-modal
+      title="电子书表单"
+      v-model:visible="modalVisible"
+      :confirm-loading="modalLoading"
+      @ok="handleModalOk"
+  >
+    <p>test</p>
+  </a-modal>
 </template>
 
 <script lang="ts">
@@ -113,11 +121,28 @@ export default defineComponent({
         size: pagination.pageSize
       });
     };
+    // -------- 表单 ---------
+    const modalVisible = ref(false);
+    const modalLoading = ref(false);
+    const handleModalOk = () => {
+      modalLoading.value = true;
+      setTimeout(() => {
+        modalVisible.value = false;
+        modalLoading.value = false;
+      }, 2000);
+    };
+
+    /**
+     * 编辑
+     */
+    const edit = () => {
+      modalVisible.value = true;
+    };
 
     onMounted(() => {
       handleQuery({
-        page : 1,
-        size : pagination.value.pageSize
+        page: 1,
+        size: pagination.value.pageSize,
       });
     });
 
@@ -126,7 +151,13 @@ export default defineComponent({
       pagination,
       columns,
       loading,
-      handleTableChange
+      handleTableChange,
+
+      edit,
+
+      modalVisible,
+      modalLoading,
+      handleModalOk
     }
   }
 });

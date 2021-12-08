60bd6bff72c03d92754e59a18ac404117ac0c791
WildThorn <1343578324@qq.com>
2021-12-08 09:24:05 +0800

    集成Ant Design Vue图标库ヾ(･∀･｀*), 命令npm install --save @ant-design/icons-vue

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,6 +4,14 @@ import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+import * as Icons from "@ant-design/icons-vue";
 
 // 集成第三方组件，一般都在main.ts里增添配置
-createApp(App).use(store).use(router).use(Antd).mount('#app');
+const  app = createApp(App);
+app.use(store).use(router).use(Antd).mount('#app');
+
+//全局使用图标
+const  icons: any = Icons;
+for (const i in icons){
+    app.component(i, icons[i]);
+}

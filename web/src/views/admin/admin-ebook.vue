e90d85e4ccc82fb97375669b131d67d3c4b26434
WildThorn <1343578324@qq.com>
2021-12-08 12:14:12 +0800

    修改电子书管理页面布局 ヾ(･∀･｀*)

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="about">
-    <h1>关于神秘的WildThorn的电子书管理</h1>
-  </div>
+
+  <a-layout>
+
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <div class="about">
+        <h1>关于神秘的WildThorn的电子书管理</h1>
+      </div>
+    </a-layout-content>
+  </a-layout>
 </template>

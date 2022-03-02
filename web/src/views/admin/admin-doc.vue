3cb33b31eaac8927f911a1a5d5497735bb594fcc
WildThorn <1343578324@qq.com>
2022-03-02 17:15:54 +0800

    文档管理增加内容预览功能ｄ(･∀･*)♪ﾟ

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -81,6 +81,11 @@
             <a-form-item>
               <a-input v-model:value="doc.sort" placeholder="顺序"/>
             </a-form-item>
+            <a-form-item>
+              <a-button type="primary" @click="handlePreviewContent()">
+                <EyeOutlined /> 内容预览
+              </a-button>
+            </a-form-item>
             <a-form-item >
               <div id="content"></div>
             </a-form-item>
@@ -88,6 +93,9 @@
         </a-col>
       </a-row>
 
+      <a-drawer width="900" placement="right" :closable="false" :visible="drawerVisible" @close="onDrawerClose">
+        <div class="wangeditor" :innerHTML="previewHtml"></div>
+      </a-drawer>
     </a-layout-content>
   </a-layout>
 
@@ -362,6 +370,17 @@ export default defineComponent({
         },
       });
     };
+// ----------------富文本预览--------------
+    const drawerVisible = ref(false);
+    const previewHtml = ref();
+    const handlePreviewContent = () => {
+      const html = editor.txt.html();
+        previewHtml.value = html;
+        drawerVisible.value = true;
+      };
+    const onDrawerClose = () => {
+        drawerVisible.value = false;
+      };
 
     onMounted(() => {
       handleQuery();
@@ -386,7 +405,11 @@ export default defineComponent({
 
       handleDelete,
 
-      treeSelectData
+      treeSelectData,
+      drawerVisible,
+      previewHtml,
+      handlePreviewContent,
+      onDrawerClose,
     }
   }
 });

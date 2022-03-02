5b0fe4c458fc1e13d268be603d4a373370cc73f2
WildThorn <1343578324@qq.com>
2022-03-02 16:11:07 +0800

    增加文档页面，点击首页电子书可以进入文档页面ヾ(≧∇≦*)ヾ

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/home.vue'
 import About from '../views/about.vue'
+import Doc from '../views/doc.vue'
 import AdminEbook from '../views/admin/admin-ebook.vue'
 import AdminCategory from '../views/admin/admin-category.vue'
 import AdminDoc from '../views/admin/admin-doc.vue'
@@ -21,6 +22,11 @@ const routes: Array<RouteRecordRaw> = [
   //   component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
   },
   {
+    path: '/doc',
+    name: 'Doc',
+    component: Doc
+  },
+  {
     path: '/admin/ebook',
     name: 'AdminEbook',
     component: AdminEbook
@@ -35,6 +41,7 @@ const routes: Array<RouteRecordRaw> = [
     name: 'AdminDoc',
     component: AdminDoc
   },
+
 ]
 
 const router = createRouter({

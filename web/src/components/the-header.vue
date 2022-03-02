5866f638467e5d3280c881b5e470542dba5f4e7c
WildThorn <1343578324@qq.com>
2022-03-02 20:33:19 +0800

    前端增加登录框ﾍ(ﾟ∀ﾟﾍ)ｱﾋｬ

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -2,7 +2,6 @@
   <a-layout-header class="header">
     <div class="logo" />
     <a-menu
-        v-model:selectedKeys="selectedKeys1"
         theme="dark"
         mode="horizontal"
         :style="{ lineHeight: '64px' }"
@@ -15,15 +14,19 @@
       </a-menu-item>
       <a-menu-item key="/admin/ebook">
         <router-link to="/admin/ebook">电子书管理</router-link>
-      </a-menu-item><a-menu-item key="/admin/category">
+      </a-menu-item>
+      <a-menu-item key="/admin/category">
         <router-link to="/admin/category">分类管理</router-link>
       </a-menu-item>
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
-      <a class="login-menu" @click="showLoginModal">
-        <span>登录</span>
-      </a>
+      <a-menu-item >
+        <a class="login-menu" @click="showLoginModal">
+          <span>登录</span>
+        </a>
+      </a-menu-item>
+
     </a-menu>
 
     <a-modal

78c0adfbe7cc41f840ee8ea577fde525af934d80
WildThorn <1343578324@qq.com>
2022-03-02 14:39:04 +0800

    增加保存文档内容功能Ｏ(≧▽≦)Ｏ

--- src/main/java/com/myspace/wiki/service/DocService.java
+++ src/main/java/com/myspace/wiki/service/DocService.java
@@ -3,8 +3,10 @@ package com.myspace.wiki.service;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.myspace.wiki.domain.Content;
 import com.myspace.wiki.domain.Doc;
 import com.myspace.wiki.domain.DocExample;
+import com.myspace.wiki.mapper.ContentMapper;
 import com.myspace.wiki.mapper.DocMapper;
 import com.myspace.wiki.request.DocQueryReq;
 import com.myspace.wiki.request.DocSaveReq;
@@ -27,6 +29,8 @@ public class DocService {
 
     @Resource
     private DocMapper docMapper;
+    @Resource
+    private ContentMapper contentMapper;
     @Resource
     private SnowFlake snowFlake;
 
@@ -82,22 +86,30 @@ public class DocService {
     }
 
     /**
-     * 保存分类
+     * 保存
      */
     public void save(DocSaveReq req) {
         Doc doc = CopyUtil.copy(req, Doc.class);
+        Content content = CopyUtil.copy(req, Content.class);
         if (ObjectUtils.isEmpty(req.getId())) {
             // 新增
             doc.setId(snowFlake.nextId());
             docMapper.insert(doc);
+
+            content.setId(doc.getId());
+            contentMapper.insert(content);
         } else {
             // 更新
             docMapper.updateByPrimaryKey(doc);
+            int count = contentMapper.updateByPrimaryKeyWithBLOBs(content);
+            if (count == 0) {
+                contentMapper.insert(content);
+                }
         }
     }
 
     /**
-     * 删除分类
+     * 删除
      * @param id
      */
     public void delete(Long id) {
--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -181,7 +181,9 @@ export default defineComponent({
     // 因为树选择组件的属性状态，会随当前编辑的节点而变化，所以单独声明一个响应式变量
     const treeSelectData = ref();
     treeSelectData.value = [];
-    const doc = ref({});
+    const doc = ref();
+    doc.value = {};
+
     const modalVisible = ref(false);
     const modalLoading = ref(false);
     // 初始化富文本
@@ -195,6 +197,7 @@ export default defineComponent({
 
     const handleSave = () => {
       modalLoading.value = true;
+      doc.value.content = editor.txt.html();
       axios.post("/doc/save", doc.value).then((response) => {
         modalLoading.value = false;
         const data = response.data; // data = commonResp

1d3a71004c7592208de3175cd0295351577a55cf
WildThorn <1343578324@qq.com>
2022-03-02 17:19:28 +0800

    打开文档页面时，默认选中第一个节点♪（＾∀＾●）ﾉ

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -9,6 +9,7 @@
               @select="onSelect"
               :replaceFields="{title: 'name', key: 'id', value: 'id'}"
               :defaultExpandAll="true"
+              :defaultSelectedKeys="defaultSelectedKeys"
           >
           </a-tree>
         </a-col>
@@ -33,6 +34,8 @@ export default defineComponent({
     const route = useRoute();
     const docs = ref();
     const html = ref();
+    const defaultSelectedKeys = ref();
+    defaultSelectedKeys.value = [];
     /**
      * 一级文档树，children属性就是二级文档
      * [{
@@ -48,29 +51,33 @@ export default defineComponent({
     level1.value = [];
 
     /**
-     * 数据查询
-     **/
-    const handleQuery = () => {
-      axios.get("/doc/all/" + route.query.ebookId).then((response) => {
-        const data = response.data;
-        if (data.success) {
-          docs.value = data.content;
-
-          level1.value = [];
-          level1.value = Tool.array2Tree(docs.value, 0);
-        } else {
-          message.error(data.message);
-        }
-      });
-    };
-    /**
      * 内容查询
      **/
     const handleQueryContent = (id: number) => {
       axios.get("/doc/find-content/" + id).then((response) => {
         const data = response.data;
         if (data.success) {
+
           html.value = data.content;
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+    /**
+     * 数据查询
+     **/
+    const handleQuery = () => {
+      axios.get("/doc/all/" + route.query.ebookId).then((response) => {
+        const data = response.data;
+        if (data.success) {
+            docs.value = data.content;
+            level1.value = [];
+            level1.value = Tool.array2Tree(docs.value, 0);
+          if (Tool.isNotEmpty(level1)) {
+              defaultSelectedKeys.value = [level1.value[0].id];
+              handleQueryContent(level1.value[0].id);
+            }
         } else {
           message.error(data.message);
         }
@@ -92,7 +99,8 @@ export default defineComponent({
     return {
       level1,
       html,
-      onSelect
+      onSelect,
+      defaultSelectedKeys
     }
   }
 });

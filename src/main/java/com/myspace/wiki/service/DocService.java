c33c815924276f00544a21500c6484ed5f68d862
WildThorn <1343578324@qq.com>
2022-03-02 15:02:57 +0800

    增加读取文档内容功能(ﾉ≧∀≦)ﾉ

--- src/main/java/com/myspace/wiki/service/DocService.java
+++ src/main/java/com/myspace/wiki/service/DocService.java
@@ -121,4 +121,14 @@ public class DocService {
         criteria.andIdIn(ids);
         docMapper.deleteByExample(docExample);
     }
+
+    /**
+     * 查找内容
+     * @param id
+     * @return
+     */
+    public String findContent(Long id) {
+        Content content = contentMapper.selectByPrimaryKey(id);
+        return content.getContent();
+    }
 }

fc53bb96d06c6d248bad41f3e096542e0b5f41c8
WildThorn <1343578324@qq.com>
2022-03-03 21:12:54 +0800

    后端增加获取统计数值接口o((*^▽^*))o

--- src/main/java/com/myspace/wiki/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/com/myspace/wiki/mapper/EbookSnapshotMapperCust.java
@@ -1,5 +1,11 @@
 package com.myspace.wiki.mapper;
+
+import com.myspace.wiki.response.StatisticResp;
+
+import java.util.List;
+
 public interface EbookSnapshotMapperCust {
 
     public void genSnapshot();
+    List<StatisticResp> getStatistic();
 }

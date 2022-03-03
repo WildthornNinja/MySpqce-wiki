1dbe95500e9ad5be936d42ae917f2f6594afff71
WildThorn <1343578324@qq.com>
2022-03-03 09:22:52 +0800

    集成redis，登录成功后将登录信息放入redis中ヾ(･∀･｀*)

--- src/main/java/com/myspace/wiki/controller/UserController.java
+++ src/main/java/com/myspace/wiki/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.myspace.wiki.controller;
 
 
+import com.alibaba.fastjson.JSONObject;
 import com.myspace.wiki.request.UserLoginReq;
 import com.myspace.wiki.request.UserQueryReq;
 import com.myspace.wiki.request.UserResetPasswordReq;
@@ -10,18 +11,27 @@ import com.myspace.wiki.response.PageResp;
 import com.myspace.wiki.response.UserLoginResp;
 import com.myspace.wiki.response.UserQueryResp;
 import com.myspace.wiki.service.UserService;
+import com.myspace.wiki.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
     @Resource
     private UserService userService;
+    @Resource
+    private RedisTemplate redisTemplate;
+    @Resource
+    private SnowFlake snowFlake;
 
     @GetMapping("/list")
     public CommonResp list(@Valid UserQueryReq req) {
@@ -57,6 +67,10 @@ public class UserController {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登录token：{}，并放入redis中", token);
+        userLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(userLoginResp), 3600 * 24, TimeUnit.SECONDS);
         resp.setContent(userLoginResp);
         return resp;
     }

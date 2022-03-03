32aad2973cfef4d5408f3aef8b430093c4b405ba
WildThorn <1343578324@qq.com>
2022-03-03 11:14:29 +0800

    前端未登录时，隐藏菜单,退出登陆时，重定向至首页(*ﾟ∀ﾟ)=3

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -70,6 +70,7 @@ import { defineComponent, ref, computed} from 'vue';
 import axios from 'axios';
 import { message } from 'ant-design-vue';
 import store from "@/store";
+import router from "@/router";
 
 declare let hexMd5: any;
 declare let KEY: any;
@@ -116,6 +117,7 @@ export default defineComponent({
         if (data.success) {
           message.success("退出登录成功！");
           store.commit("setUser", {});
+          router.push('/')
         } else {
           message.error(data.message);
         }

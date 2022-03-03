78d1a7fcca38ed0865661fb9f414f264a7a4d5df
WildThorn <1343578324@qq.com>
2022-03-03 09:53:12 +0800

    前端显示登陆昵称☆´∀｀☆

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,6 +1,8 @@
 <template>
   <a-layout-header class="header">
     <div class="logo" />
+    <a-row>
+      <a-col :span="12">
     <a-menu
         theme="dark"
         mode="horizontal"
@@ -21,13 +23,18 @@
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
-      <a-menu-item >
-        <a class="login-menu" @click="showLoginModal">
-          <span>登录</span>
-        </a>
-      </a-menu-item>
 
     </a-menu>
+      </a-col>
+      <a-col :span="12">
+        <a class="login-menu" v-show="user.id">
+          <span>您好吖:{{user.name}}</span>
+        </a>
+        <a class="login-menu" v-show="!user.id" @click="showLoginModal">
+          <span>登陆</span>
+        </a>
+      </a-col>
+    </a-row>
 
     <a-modal
         title="登录"
@@ -57,9 +64,13 @@ declare let KEY: any;
 export default defineComponent({
   name: 'the-header',
   setup () {
+    // 登录后保存
+    const user = ref();
+    user.value = {};
+    // 用来登录
     const loginUser = ref({
       loginName: "test",
-      password: "test"
+      password: "test123"
     });
     const loginModalVisible = ref(false);
     const loginModalLoading = ref(false);
@@ -78,6 +89,7 @@ export default defineComponent({
         if (data.success) {
           loginModalVisible.value = false;
           message.success("登录成功！");
+          user.value = data.content;
         } else {
           message.error(data.message);
         }
@@ -89,7 +101,8 @@ export default defineComponent({
       loginModalLoading,
       showLoginModal,
       loginUser,
-      login
+      login,
+      user
     }
   }
 });

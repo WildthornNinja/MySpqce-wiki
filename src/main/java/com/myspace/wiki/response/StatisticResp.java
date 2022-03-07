b970ccde8444dcc2af879a88d35e30f5184d8d36
WildThorn <1343578324@qq.com>
2022-03-07 14:55:08 +0800

    展示30天趋势图●ヽ(ﾟ∀ﾟ)ﾉ●

--- src/main/java/com/myspace/wiki/response/StatisticResp.java
+++ src/main/java/com/myspace/wiki/response/StatisticResp.java
@@ -1,9 +1,12 @@
 package com.myspace.wiki.response;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 public class StatisticResp {
+    @JsonFormat(pattern="MM-dd", timezone = "GMT+8")
     private Date date;
 
     private int viewCount;
